62b44867f8013393d29b83ccdbd1da8ef57c692b
Selin Dayioglu <63448943+selindayioglu@users.noreply.github.com>
2021-08-23 18:15:38 +0300

    Lesson 8: Array Function

---
+++ Array Functions/array-functions.js
@@ -0,0 +1,34 @@
+const users = [
+  { name: "Hasan", age: 22 },
+  { name: "Selin", age: 21 },
+];
+
+// push
+// users.push("karpuz");
+// console.log(users);
+
+// map
+// users.map((item) => {
+//   console.log(item.name);
+// });
+
+// find
+// const rslt = users.find((item) => item.name === "Hasan" && item.age > 20);
+// console.log(rslt);
+
+// filter
+// const filtered = users.find(({ name }) => name === "Hasan");
+// console.log(filtered);
+
+//some true veya false döndürür
+// const some = users.some((item) => item.age === 26);
+// console.log(some);
+
+// every tüm elemanları kontrol eder hepsi için doğru ise true döner
+// const every = users.every((item) => item.age > 21);
+// console.log(every);
+
+//includes içinde var mı yok mu onu bulur
+// const names = ["Jack", "Harry"];
+// const isIncluded = names.includes("Harry");
+// console.log(isIncluded);
